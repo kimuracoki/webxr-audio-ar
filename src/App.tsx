c42311e69371bd1f2b565dd38d35404873c72001
tsuzuki <tsuzuki@autoproject.biz>
2025-03-18 16:59:40 +0900

    Add overray

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { ImageTexture } from "./components/ImageTexture";
+import { Overlay } from "./components/OverRay";
 
 function App() {
   return (
     <>
       <ImageTexture />
+      <Overlay />
     </>
   );
 }
